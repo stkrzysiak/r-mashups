6d703864ed149760b2efb4740558f54a42423f6d
Steve Krzysiak <stkrzysiak@gmail.com>
2016-08-17 04:12:59 -0500

    Get the top of all time

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 "use strict";
 const reddit = require('redwrap');
 const ytdl = require('youtube-dl');
+const chalk = require('chalk');
+let counter = 0;
 
-reddit.r('mashups').top().limit(30).exe(function(err, data, res){
+reddit.r('mashups/top').from('all').sort('top').limit(100).exe(function(err, data, res){
   for (let post of data.data.children) {
-   console.log(post.data.domain);
-    if(post.data.domain === 'soundcloud.com') {
+    counter++;
+   console.log(chalk.blue(`#${counter} ${post.data.title} ${post.data.domain}`));
+    if(post.data.domain === 'youtube.com') {
+      console.log(`Fixing to DL ${post.data.title} ${post.data.domain}`);
       ytdl.exec(post.data.url, ['-x', '--audio-format', 'mp3'], {}, function(err, output) {
-        if (err) throw err;
-        console.log(output.join('\n'));
+        if (err) {
+          console.log(chalk.red(err));
+        } else {
+          console.log(chalk.green(output.join('\n')));
+        }
       });
    }
 }
